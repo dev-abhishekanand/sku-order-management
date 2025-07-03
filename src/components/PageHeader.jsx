2d532d3dfc66d2161a843d16edd50bb914e197f6
dev-abhishekanand <abhishek.anand.7601@gmail.com>
2025-07-03 17:49:47 +0530

    Alignment fix

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -1,10 +1,14 @@
 const PageHeader = ({ title, leftActions, rightActions, children }) => (
-    <div className="flex items-center justify-between mb-6">
-        <div className="flex items-center gap-2">{leftActions}</div>
-        <h1 className="font-bold text-2xl px-4 py-2 bg-gray-700 text-white rounded-2xl">
-            {title}
-        </h1>
-        <div className="flex items-center gap-2">{rightActions}</div>
+    <div className="flex flex-col gap-2 sm:flex-row sm:items-center sm:justify-between mb-6">
+        <div className="flex items-center gap-4">
+            {leftActions}
+            <h1 className="font-bold text-2xl px-4 py-2 bg-gray-700 text-white rounded-2xl">
+                {title}
+            </h1>
+        </div>
+        {rightActions && (
+            <div className="flex items-center gap-2">{rightActions}</div>
+        )}
         {children}
     </div>
 );
